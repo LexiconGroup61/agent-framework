87c80609f92af1f4fbf5c541ae02dd63d523bf5d
tobiasengberg <tobias.engberg@exilic.se>
2026-09-08 11:51:48 +0200

    Add RAG

---
+++ Document/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Extensions.AI;
+using OpenAI;
+using OpenAI.Chat;
+
+// // Setting up the AI agentChat
+
+DotNetEnv.Env.Load();
+Console.WriteLine(Environment.GetEnvironmentVariable("OPENAIAPI_KEY"));
+var client = new OpenAIClient(Environment.GetEnvironmentVariable("OPENAIAPI_KEY"))
+    .GetChatClient("GPT-4.1")
+    .AsIChatClient();
+
+// Reading the pdf
+
+var byteData = await File.ReadAllBytesAsync("ddd.pdf");
+var readOnlyData = new ReadOnlyMemory<byte>(byteData);
+
+// Execute the query
+var contents = new List<AIContent>
+{
+    new TextContent("What does Evans mean with Granularity?"),
+    new DataContent(readOnlyData, "application/pdf")
+};
+
+var chatMessage = new Microsoft.Extensions.AI.ChatMessage(ChatRole.User, contents);
+
+var result = await client.AsAIAgent().RunAsync(chatMessage);
+
+Console.WriteLine(result.Text);
+
---
+++ RAG/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.ClientModel;
+using Microsoft.Extensions.AI;
+using OpenAI;
+using Microsoft.SemanticKernel.Text;
+
+var client = new OpenAIClient(
+    new ApiKeyCredential("text"),
+    new OpenAIClientOptions
+    {
+        Endpoint = new Uri("http://127.0.0.1:1234/v1")
+    })
+    .GetEmbeddingClient("text-embedding-nomic-embed-text-v1.5-embedding");
+    
+
+string plainText = await File.ReadAllTextAsync("text.txt");
+#pragma warning disable SKEXP0050
+var chunkedText = TextChunker.SplitPlainTextParagraphs(
+#pragma warning restore SKEXP0050
+    lines: plainText.Split('\n'),
+        maxTokensPerParagraph: 200,
+        overlapTokens: 30
+    );
+
+var embeddings = await client.GenerateEmbeddingsAsync(chunkedText);
+
+foreach (var embedding in embeddings.Value)
+{
+    var subjectNumbers = embedding.ToFloats().ToArray();
+    for (int i = 0; i < subjectNumbers.Length; i++)
+    {
+        Console.Write(subjectNumbers[i] + "_" );
+    }
+
+    Console.WriteLine();
+}
+
+
