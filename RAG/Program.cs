af3bfd6a4d984f8be91c2b3ae46146452971a5ba
tobiasengberg <tobias.engberg@exilic.se>
2026-09-09 10:44:18 +0200

    Add vector store

--- RAG/Program.cs
+++ RAG/Program.cs
@@ -3,6 +3,8 @@ using System.ClientModel;
 using Microsoft.Extensions.AI;
 using OpenAI;
 using Microsoft.SemanticKernel.Text;
+using Npgsql;
+using Pgvector;
 
 var client = new OpenAIClient(
     new ApiKeyCredential("text"),
@@ -24,15 +26,41 @@ var chunkedText = TextChunker.SplitPlainTextParagraphs(
 
 var embeddings = await client.GenerateEmbeddingsAsync(chunkedText);
 
-foreach (var embedding in embeddings.Value)
-{
-    var subjectNumbers = embedding.ToFloats().ToArray();
-    for (int i = 0; i < subjectNumbers.Length; i++)
-    {
-        Console.Write(subjectNumbers[i] + "_" );
-    }
+// foreach (var embedding in embeddings.Value)
+// {
+//     var subjectNumbers = embedding.ToFloats().ToArray();
+//     for (int i = 0; i < subjectNumbers.Length; i++)
+//     {
+//         Console.Write(subjectNumbers[i] + "_" );
+//     }
+//
+//     Console.WriteLine();
+// }
 
-    Console.WriteLine();
+var dataSourceBuilder =
+    new NpgsqlDataSourceBuilder("Host=localhost;Username=postgres;Password=postgres;Database=pgvector");
+dataSourceBuilder.UseVector();
+await using var dataSource = dataSourceBuilder.Build();
+
+var connection = await dataSource.OpenConnectionAsync();
+
+await using (var cmd = new NpgsqlCommand("CREATE EXTENSION IF NOT EXISTS vector", connection))
+{
+    await cmd.ExecuteNonQueryAsync();
 }
 
+await using (var cmd = new NpgsqlCommand(
+                 "CREATE TABLE IF NOT EXISTS documents (id serial PRIMARY KEY, embedding vector(768))", connection))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
 
+await using (var cmd = new NpgsqlCommand("INSERT INTO documents (embedding) VALUES ($1)", connection))
+{
+    foreach (var embedding in embeddings.Value)
+    {
+        cmd.Parameters.AddWithValue(new Vector(embedding.ToFloats()));
+        await cmd.ExecuteNonQueryAsync();
+
+    }
+}
