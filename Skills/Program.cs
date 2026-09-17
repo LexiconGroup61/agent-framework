6893e59e1d1a9294f8509987b411731af78ded3c
tobiasengberg <tobias.engberg@exilic.se>
2026-09-17 10:46:02 +0200

    Skills

---
+++ Skills/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ClientModel;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI;
+using OpenAI.Chat;
+using OpenAI.Responses;
+
+var skillsProvider = new AgentSkillsProvider(
+    skillPath: Path.Combine(AppContext.BaseDirectory, "skills")
+);
+
+DotNetEnv.Env.Load();
+var agent = new OpenAIClient(Environment.GetEnvironmentVariable("OPENAIAPI_KEY"))
+    .GetChatClient("gpt-6-astra")
+    .AsAIAgent(new ChatClientAgentOptions()
+    {
+        ChatOptions = new() { Instructions = "You are a helpful assistant for Good-Corp, which is based in Sweden and are a commercial business." },
+        AIContextProviders = [skillsProvider]
+    });
+    
+var response = agent.RunAsync("What rate of VAT should Good-Corp add for the aerobics class sold?");
+
+Console.WriteLine(response.Result.Text);
---
+++ Skills/skills/VAT-rules/SKILL.md
@@ -0,0 +1,8 @@
+---
+name: VAT-rules
+description: >-
+    "Determines rates of VAT for Good-Corp as either 25%, 12%, 6% or VAT-exempt. Use in order to find out about VAT rates on certain goods and services, and how much VAT should be charged when selling them."
+---
+
+# Instructions
+1. Check the rules available at references/VAT-rules.md
